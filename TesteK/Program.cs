b9dc0dd0d6e21249cc2db9b24a147e7d3282ee13
Natan Lopes <natanlp98@hotmail.com>
2018-01-10 12:18:33 -0200

    Projeto iniciado, monstrando a mensagem Ola mundo

---
+++ TesteK/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace TesteK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
